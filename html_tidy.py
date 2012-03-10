0b9d1efc0914163a905545520cd00a2d35813e65
welovewordpress <info@welovewordpress.de>
2012-03-10 12:59:52 +0100

    added windows support (beta)

--- html_tidy.py
+++ html_tidy.py
@@ -1,24 +1,32 @@
 import sublime, sublime_plugin, re, os
-import pprint
 
 class HtmlTidyCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         print('HtmlTidy: invoked on file: %s' % ( self.view.file_name() ) )
 
-        # path to temp file
-        tmpfile = '/tmp/htmltidy-sublime-buffer.php'
-        phppath = '/usr/bin/php'
-
         # path to plugin - <sublime dir>/Packages/PhpTidy
         pluginpath = sublime.packages_path() + '/HtmlTidy'
-
-        # set script and check if it exists
         scriptpath = pluginpath + '/tidy.php'
+
+        # path to temp file
+        tmpfile = '/tmp/htmltidy-sublime-buffer.tmp'
+        phppath = '/usr/bin/php'
+
+        # set different paths for php and temp file on windows
+        if sublime.platform() == 'windows':
+            tmpfile = pluginpath + '/htmltidy-sublime-buffer.tmp'
+            phppath = 'php.exe'
+            retval = os.system( '%s -v' % ( phppath ) )
+            if not retval == 0:
+                sublime.error_message('HtmlTidy cannot find %s. Make sure it is available in your PATH.' % (phppath))
+                return
+
+        # check if tidy.php is found
         if not os.path.exists( scriptpath ):
             sublime.error_message('HtmlTidy cannot find the script at %s.' % (scriptpath))
             return
 
-        # get current buffer
+        # get current buffer content
         bufferLength  = sublime.Region(0, self.view.size())
         bufferContent = self.view.substr(bufferLength).encode('utf-8')
 
@@ -28,31 +36,31 @@ class HtmlTidyCommand(sublime_plugin.TextCommand):
         fileHandle.close() 
         print('HtmlTidy: buffer written to tmpfile: %s' % (tmpfile))
 
-        # supported tidy options
+        # get supported tidy options
         supported_options = self.get_supported_options()
-        # pprint.pprint(supported)
 
         # apply settings    
         settings = sublime.load_settings('HtmlTidy.sublime-settings')    
         # settings = self.view.settings()
         # indent_spaces = int(settings.get('htmltidy-indent-spaces', 4))
 
+        # build command line arguments
+        # leaves out default values
         args = ''
         for option in supported_options:
-                default_value = supported_options[option]
-                if default_value == True : default_value = 1
-                if default_value == False: default_value = 0
-                custom_value = settings.get('htmltidy-'+option)
-                if custom_value == True : custom_value = 1
-                if custom_value == False: custom_value = 0
-                if not custom_value == None and not custom_value == default_value:
-                        # print "HtmlTidy: setting ", option, ": ", default_value, "->", custom_value
-                        args = args + (" --%s=%s" % (option,custom_value))
-
+            default_value = supported_options[option]
+            if default_value == True : default_value = 1
+            if default_value == False: default_value = 0
+            custom_value = settings.get('htmltidy-'+option)
+            if custom_value == True : custom_value = 1
+            if custom_value == False: custom_value = 0
+            if not custom_value == None and not custom_value == default_value:
+                # print "HtmlTidy: setting ", option, ": ", default_value, "->", custom_value
+                args = args + (" --%s=%s" % (option,custom_value))
 
         # call tidy.php on tmpfile
-        print('HtmlTidy: calling script: %s "%s" %s' % ( phppath, scriptpath, args ) )
-        retval = os.system( '%s "%s" %s' % ( phppath, scriptpath, args ) )
+        print('HtmlTidy: calling script: %s "%s" "%s" %s' % ( phppath, scriptpath, tmpfile, args ) )
+        retval = os.system( '%s "%s" "%s" %s' % ( phppath, scriptpath, tmpfile, args ) )
         if retval != 0:
             print('HtmlTidy: script returned: %s' % (retval))
             if retval == 32512:
@@ -60,7 +68,7 @@ class HtmlTidyCommand(sublime_plugin.TextCommand):
                 return
             else:
                 sublime.error_message('There was an error calling the script at %s. Return value: %s' % (scriptpath,retval))
-
+                return
 
         # read tmpfile and delete
         fileHandle = open ( tmpfile, 'r' ) 
@@ -73,28 +81,11 @@ class HtmlTidyCommand(sublime_plugin.TextCommand):
         self.view.replace(edit, bufferLength, self.fixup(newContent))
 
 
-        # reminder: different ways of logging errors in sublime
-        #
-        # sublime.status_message('opening file: %s' % (FILE))
-        # sublime.error_message(tmpfile)
-        # self.show_error_panel(self.fixup(tmpfile))
-
-
-    # Error panel & fixup from external command
-    # https://github.com/technocoreai/SublimeExternalCommand
-    def show_error_panel(self, stderr):
-        panel = self.view.window().get_output_panel("php_tidy_errors")
-        panel.set_read_only(False)
-        edit = panel.begin_edit()
-        panel.erase(edit, sublime.Region(0, panel.size()))
-        panel.insert(edit, panel.size(), stderr)
-        panel.set_read_only(True)
-        self.view.window().run_command("show_panel", {"panel": "output.php_tidy_errors"})
-        panel.end_edit(edit)
-
+    # Fixup from external command
     def fixup(self, string):
         return re.sub(r'\r\n|\r', '\n', string.decode('utf-8'))
 
+    # get supported options and default values
     def get_supported_options(self):
         return {
             'show-body-only' : False,
